b1460ad05773ff06dfd9789e73f098bde0c9570a
elkhaddari.ayoub4 <elkhaddari.ayoub4@gmail.com>
2021-03-08 13:08:18 +0100

    test for to me

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 // part 1 decorator
 @Component({
@@ -6,12 +7,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.scss']
 })
-// class 
+// class
 export class ServiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+  closeResult = '';
 
   ngOnInit(): void {
   }
+
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
 
 }
